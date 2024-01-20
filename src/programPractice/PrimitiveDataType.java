e046b8f1b944bf6547b108e8924456a0dbc444f3
sanjaikanth <sanjaikanthb.techymeet@gmail.com>
2024-01-20 23:09:10 +0530

    primitive ata type code sample

---
+++ src/programPractice/PrimitiveDataType.java
@@ -0,0 +1,26 @@
+package programPractice;
+
+public class PrimitiveDataType {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		boolean attendance=true;
+		System.out.println(attendance);
+		char letter='a';
+		System.out.println(letter);
+		byte a=10;
+		System.out.println(a);
+		short b=1233;
+		System.out.println(b);
+		int c=10;
+		System.out.println(c);
+		long d=123232;
+		System.out.println(d);
+		float num=1.232f;
+		System.out.println(num);
+		double num1=12.3634;
+		System.out.println(num1);
+
+	}
+
+}
