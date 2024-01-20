0824863249412f850daa3e17cc76458188212bb7
sanjaikanth <sanjaikanthb.techymeet@gmail.com>
2024-01-21 00:04:17 +0530

    Control flow statement code

---
+++ src/programPractice/DecisionMakingStatement.java
@@ -0,0 +1,21 @@
+package programPractice;
+
+public class DecisionMakingStatement {
+
+	public static void main(String[] args) {
+		
+		String fruit ="Apple";
+
+		switch(fruit) {
+		case "Orange":
+		System.out.println("fruit Orange");
+		break;
+		case "Apple":
+		System.out.println("fruit Apple");
+		break;
+		default:
+		System.out.println("Not a fruit");
+		}
+	}
+
+}
